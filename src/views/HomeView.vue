710ccf4e70a6441a52af30ed02b87121036c7a9b
nico <nicolascfv@hotmail.com>
2025-02-09 23:02:28 +0100

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,117 @@
+<script setup lang="ts">
+import HomeCard from '@/components/HomeCard.vue'
+</script>
+
+<template>
+  <div class="!p-8">
+    <button tpye="button" class="text-white text-2xl font-semibold hover:underline cursor-pointer">
+      Podcast to make you smarter
+    </button>
+
+    <div class="!py-1.5"></div>
+    <div class="flex items-center">
+      <HomeCard
+        image="https://picsum.photos/id/30/300/300"
+        tittle="title here"
+        subTittle="subtittle here"
+      />
+      <HomeCard
+        image="https://picsum.photos/id/42/300/300"
+        tittle="title here"
+        subTittle="subtittle here"
+      />
+      <HomeCard
+        image="https://picsum.photos/id/69/300/300"
+        tittle="title here"
+        subTittle="subtittle here"
+        class="md:block hidden"
+      />
+      <HomeCard
+        image="https://picsum.photos/id/77/300/300"
+        tittle="title here"
+        subTittle="subtittle here"
+        class="lg:block hidden"
+      />
+      <HomeCard
+        image="https://picsum.photos/id/90/300/300"
+        tittle="title here"
+        subTittle="subtittle here"
+        class="xl:block hidden"
+      />
+    </div>
+  </div>
+  <div class="!p-8">
+    <button tpye="button" class="text-white text-2xl font-semibold hover:underline cursor-pointer">
+      Recomended Songs
+    </button>
+
+    <div class="!py-1.5"></div>
+    <div class="flex items-center">
+      <HomeCard
+        image="https://picsum.photos/id/30/300/300"
+        tittle="title here"
+        subTittle="subtittle here"
+      />
+      <HomeCard
+        image="https://picsum.photos/id/42/300/300"
+        tittle="title here"
+        subTittle="subtittle here"
+      />
+      <HomeCard
+        image="https://picsum.photos/id/69/300/300"
+        tittle="title here"
+        subTittle="subtittle here"
+        class="md:block hidden"
+      />
+      <HomeCard
+        image="https://picsum.photos/id/77/300/300"
+        tittle="title here"
+        subTittle="subtittle here"
+        class="lg:block hidden"
+      />
+      <HomeCard
+        image="https://picsum.photos/id/90/300/300"
+        tittle="title here"
+        subTittle="subtittle here"
+        class="xl:block hidden"
+      />
+    </div>
+  </div>
+  <div class="!p-8">
+    <button tpye="button" class="text-white text-2xl font-semibold hover:underline cursor-pointer">
+      Recomended radio
+    </button>
+
+    <div class="!py-1.5"></div>
+    <div class="flex items-center">
+      <HomeCard
+        image="https://picsum.photos/id/30/300/300"
+        tittle="title here"
+        subTittle="subtittle here"
+      />
+      <HomeCard
+        image="https://picsum.photos/id/42/300/300"
+        tittle="title here"
+        subTittle="subtittle here"
+      />
+      <HomeCard
+        image="https://picsum.photos/id/69/300/300"
+        tittle="title here"
+        subTittle="subtittle here"
+        class="md:block hidden"
+      />
+      <HomeCard
+        image="https://picsum.photos/id/77/300/300"
+        tittle="title here"
+        subTittle="subtittle here"
+        class="lg:block hidden"
+      />
+      <HomeCard
+        image="https://picsum.photos/id/90/300/300"
+        tittle="title here"
+        subTittle="subtittle here"
+        class="xl:block hidden"
+      />
+    </div>
+  </div>
+</template>
---
+++ src/views/SearchView.vue
@@ -0,0 +1,27 @@
+<script setup>
+import CategorySelect from '@/components/CategorySelect.vue'
+</script>
+
+<template>
+  <div>
+    <div class="!p-8">
+      <button
+        tpye="button"
+        class="text-white text-2xl font-semibold hover:underline cursor-pointer"
+      >
+        Podcast to make you smarter
+      </button>
+
+      <div class="!py-1.5"></div>
+      <div class="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-6">
+        <CategorySelect category="podcast" image="https://picsum.photos/id/10/300/300" />
+        <CategorySelect category="audioBooks" image="https://picsum.photos/id/5/300/300" />
+        <CategorySelect category="Made for you" image="https://picsum.photos/id/99/300/300" />
+        <CategorySelect category="podcast" image="https://picsum.photos/id/10/400/300" />
+        <CategorySelect category="New release" image="https://picsum.photos/id/222/300/300" />
+        <CategorySelect category="AudioBooks" image="https://picsum.photos/id/89/300/300" />
+        <CategorySelect category="Books" image="https://picsum.photos/id/98/300/300" />
+      </div>
+    </div>
+  </div>
+</template>
